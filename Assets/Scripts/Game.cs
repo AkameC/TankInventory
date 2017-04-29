662bbbeeafbcf47fad3b6c8870f19b8256f98f91
AkameC <almmohd@yahoo.com>
2017-04-29 12:16:53 -0400

    Create a simulation of inventory drawdown as a tank is being discharged.
    A tank containing a fluid (measured in barrels) is being emptied at a
    fix rate (measured in barrels per day).

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour {
+
+	float TankInventory;
+	public Text TankInventoryText;
+	float Timer = 4f;
+	float CurrentTimer = 0;
+	bool StartTimer;
+	float Throughput;
+	//Throughput is in barrels per day
+	//Tank inventory is in barrels
+
+	// Use this for initialization
+	void Start () {
+
+		TankInventory = 720000;
+		Throughput = 150000;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (StartTimer) {
+			CurrentTimer += Time.deltaTime;
+			InventoryOnClick ();
+		}
+
+	}
+
+	public void InventoryOnClick ()
+	{
+		Timer = Time.deltaTime;
+		Debug.Log (Timer);
+
+		if (!StartTimer) {
+			StartTimer = true;
+		}
+		CalculateInventory ();
+	}
+
+	void CalculateInventory ()
+	{
+		TankInventory -= Timer * Throughput / (60 * 60 * 24);
+		TankInventoryText.text = TankInventory.ToString ();
+		Debug.Log (TankInventory);
+	}
+}
